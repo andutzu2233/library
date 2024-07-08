545ce73c5d5b3ed460f89685d98a5bbcb3b2eda0
AndreiRes <andrei.resmerita@caseware.com>
2024-07-08 19:54:49 +0300

    Revert "Library management project"
    This reverts commit ea45dffbe6b49b3bbda6284a942f3a72b9bce42c.

--- src/main/java/com/andrei/resmerita/library_mangement_system/exception/AuthorAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.andrei.resmerita.library_mangement_system.exception;
-
-public class AuthorAlreadyExistsException extends RuntimeException {
-    public AuthorAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/andrei/resmerita/library_mangement_system/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.andrei.resmerita.library_mangement_system.exception;
-
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
-
